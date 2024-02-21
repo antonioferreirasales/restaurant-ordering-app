ea2e9e9f0a026306a58299733c176a2474fe881d
Antônio Sales <antoniosales1003@gmail.com>
2024-02-21 13:40:30 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { Main } from './styles/styles'
+import { Header, Main } from './styles/styles'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Main>
-    
+        <Header>
+          <div>
+
+          <h1>Jimmy's Dinner</h1>
+          <h2>The best burgers and pizzas in town</h2>
+          </div>
+        </Header>
+        <div>Teste2</div>
       </Main>
       <GlobalStyle />
     </ThemeProvider>
---
+++ src/styles/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+  border: 1px solid ${props => props.theme['pink-500']};
+  margin: 2rem auto;
+  width: 37.5rem;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Header = styled.header`
+  div {
+    padding: 2rem 0;
+    background-image: url("src/assets/hamburger-background.png");
+    background-repeat: no-repeat;
+    background-size: contain;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+  }
+
+  h1, h2 {
+    font-weight: 400;
+    padding-left: 2.80rem;
+  }
+
+  h1 {
+    font-size: 2.43rem;
+    padding-top: 1rem;
+    line-height: 1.5rem;
+  }
+
+  h2 {
+    font-size: 1.4rem;
+    padding-bottom: 2.2rem;
+    line-height: .86rem;
+  }
+`
+
+export const Menu = styled.section`
+
+`
